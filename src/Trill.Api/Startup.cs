34cdce16956938a763003daa97f82dd93e8ee851
Piotr Gankiewicz <piotr.gankiewicz@gmail.com>
2021-05-05 14:26:21 +0200

    Stories endpoints

--- src/Trill.Api/Startup.cs
+++ src/Trill.Api/Startup.cs
@@ -1,7 +1,7 @@
 using System;
 using System.Collections.Generic;
 using System.Linq;
-using System.Threading.Tasks;
+using System.Net;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
@@ -38,6 +38,38 @@ namespace Trill.Api
                 endpoints.MapGet("/", async context =>
                 {
                     await context.Response.WriteAsync("Hello World!");
+                });
+
+                endpoints.MapGet("stories", async context =>
+                {
+                    await context.Response.WriteAsJsonAsync(_stories);
+                });
+                
+                endpoints.MapGet("stories/{storyId:guid}", async context =>
+                {
+                    var storyId = Guid.Parse(context.Request.RouteValues["storyId"].ToString());
+                    var story = _stories.SingleOrDefault(x => x.Id == storyId);
+                    if (story is null)
+                    {
+                        context.Response.StatusCode = StatusCodes.Status404NotFound;
+                        return;
+                    }
+
+                    await context.Response.WriteAsJsonAsync(story);
+                });
+                
+                endpoints.MapPost("stories", async context =>
+                {
+                    var story = await context.Request.ReadFromJsonAsync<Story>();
+                    if (story is null)
+                    {
+                        context.Response.StatusCode = StatusCodes.Status400BadRequest;
+                        return;
+                    }
+
+                    _stories.Add(story);
+                    context.Response.Headers.Add(HttpResponseHeader.Location.ToString(), $"stories/{story.Id}");
+                    context.Response.StatusCode = StatusCodes.Status201Created;
                 });
             });
         }
