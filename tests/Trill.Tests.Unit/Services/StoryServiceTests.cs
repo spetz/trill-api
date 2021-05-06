5c9b66fd9471bb7397d5d39418b660db62be5bd6
Piotr Gankiewicz <piotr.gankiewicz@gmail.com>
2021-05-06 17:16:35 +0200

    unit tests

--- tests/Trill.Tests.Unit/Services/StoryServiceTests.cs
+++ tests/Trill.Tests.Unit/Services/StoryServiceTests.cs
@@ -4,8 +4,10 @@ using System.Threading.Tasks;
 using Microsoft.Extensions.Logging;
 using NSubstitute;
 using Shouldly;
+using Trill.Core.App.Commands;
 using Trill.Core.App.Services;
 using Trill.Core.Domain.Entities;
+using Trill.Core.Exceptions;
 using Trill.Core.Repositories;
 using Xunit;
 
@@ -26,6 +28,28 @@ namespace Trill.Tests.Unit.Services
             // Assert
             story.ShouldNotBeNull();
             await _storyRepository.Received(1).GetAsync(storyId);
+        }
+        
+        [Fact]
+        public async Task add_should_succeed_given_valid_data()
+        {
+            // Arrange
+            var command = new SendStory(Guid.NewGuid(), "test", "Lorem ipsum", "user1", new[] {"tag1", "tag2"});
+            
+            // Act
+            await _storyService.AddAsync(command);
+            
+            // Assert
+            await _storyRepository.Received(1).AddAsync(Arg.Is<Story>(x => x.Id == command.Id));
+        }
+        
+        [Fact]
+        public async Task add_should_fail_given_missing_author()
+        {
+            var command = new SendStory(Guid.NewGuid(), "Test", "Lorem ipsum", string.Empty, new[] {"tag1", "tag2"});
+            var exception = await Record.ExceptionAsync(async () => await _storyService.AddAsync(command));
+            exception.ShouldNotBeNull();
+            exception.ShouldBeOfType<MissingAuthorException>();
         }
     
         private readonly IStoryRepository _storyRepository;
